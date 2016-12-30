3d46a573dea76214dcea64ef516db7457177417b
Jon Poister <jonathan.poister@mullenmail.com>
2016-12-29 23:35:02 -0700

    This is where I am leaving off for a while.

--- TheGameOfChoices/src/SecondNight.java
+++ TheGameOfChoices/src/SecondNight.java
@@ -7,5 +7,14 @@ public class SecondNight
 	{
 		final JFrame frame = new JFrame();
 		JOptionPane.showMessageDialog(frame, "Chapter 3");
+		p.print("\nI am still a little shooken up from the previous day's events and I can't fall asleep.");
+		p.print("\nI can hear my parents downstairs with my mom yelling at my dad and I don't know what to do.");
+		p.print("\nI don't really have a best friend that I can go to for help on this and I am just starting to feel down about myself.");
+		p.print("\nI suddenly heard a very loud slap from downstais and suddenly there was silence.");
+		p.print("\nThat night my dad slept on the couch and my mom slept with a bruise on the side of her cheek upstairs.");
+		p.print("\nI didn't sleep at all that night.");
+		p.print("\nWhatever has been going on is effecting our family.  I don't know what to do.");
 	}
 }
+
+//this is where I am going to leave off...for a while
